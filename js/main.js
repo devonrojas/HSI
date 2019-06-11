add752272ea6d1b3eb99c129a2b322a886af45c3
Devon Rojas <devonmrojas@gmail.com>
2019-06-11 14:21:17 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,6 +19,8 @@
       btn.classList.remove("eng");
       btn.classList.remove("button-secondary");
       btn.classList.add("button-tertiary");
+      // Persist user's language selection
+      sessionStorage.setItem("language", "spanish");
     } else {
       document.querySelectorAll(".english").forEach(el => {
         el.classList.remove("hidden");
@@ -29,8 +31,15 @@
       btn.classList.add("eng");
       btn.classList.add("button-secondary");
       btn.classList.remove("button-tertiary");
+      // Persist user's language selection
+      sessionStorage.setItem("language", "english");
     }
   });
+
+  // Persist user's language selection
+  if (sessionStorage.getItem("language") == "spanish") {
+    btn.click();
+  }
 
   window.onscroll = e => {
     let scroll = document.documentElement.scrollTop || document.body.scrollTop;
@@ -43,6 +52,7 @@
         el.classList.remove("hide");
       });
     }
+    animateValues();
   };
 
   document.querySelectorAll(".grid.team .team-member").forEach(el => {
@@ -83,4 +93,57 @@ const scrollTo = function(to, duration) {
       }
     };
   animateScroll();
+};
+
+const countUp = function(to, el, duration) {
+  console.log(to);
+  const startDate = +new Date(),
+    start = 0,
+    change = to,
+    // t = current time
+    // b = start value
+    // c = change in value
+    // d = duration
+    easeInOutQuad = function(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    },
+    animateCount = function() {
+      const currentDate = +new Date();
+      const currentTime = currentDate - startDate;
+      el.innerHTML = parseInt(
+        easeInOutQuad(currentTime, start, change, duration)
+      );
+      if (currentTime <= duration) {
+        requestAnimationFrame(animateCount);
+      } else {
+        el.innerHTML = to;
+      }
+    };
+  animateCount();
+};
+
+function animateValues() {
+  Array.from(document.querySelectorAll(".home-infographics .stat > span"))
+    .concat(
+      Array.from(document.querySelectorAll(".infographic .statistic > span"))
+    )
+    .forEach(item => {
+      const num = item.innerHTML;
+      if (isInViewport(item) && !item.classList.contains("scrolled")) {
+        countUp(num, item, 5000);
+        item.classList.add("scrolled");
+      }
+    });
+}
+
+const isInViewport = function(el) {
+  let bounding = el.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight)
+  );
 };
